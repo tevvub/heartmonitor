8ce9c27e85682444a8272e4aa5aa5bdd295c48e8
teyyub <example@gmail.com>
2020-02-06 19:11:22 +0400

    finish

--- src/js/script.js
+++ src/js/script.js
@@ -53,5 +53,49 @@ $(document).ready(function(){
             $('.overlay, #order').fadeIn('slow');
         })
     });
-    
-  });
+
+
+
+    function valideForms(form) {
+        $(form).validate({
+            rules: {
+                name: "required",
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: "Пожалуйста, введите свое имя",
+                phone: "Введите номер телефона",
+                email: {
+                  required: "Пожалуйста, введите свою почту",
+                  email: "Неправильно введен адрес почты"
+                }
+            }
+        });
+    };
+
+    valideForms('#consultation-form')
+    valideForms('#consultation form')
+    valideForms('#order form')
+
+    $('input[name=phone]').mask("+7 (999) 999-99-99")
+
+    $('form').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+
+            $('form').trigger('reset');
+        });
+        return false;
+    });
+});
